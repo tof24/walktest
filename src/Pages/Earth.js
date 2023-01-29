19952d5ac5d0a89fa724f144871961c54caefe04
André Domingues <andremrmd@ua.pt>
2023-01-29 05:26:15 +0000

    home links

--- src/Pages/Earth.js
+++ src/Pages/Earth.js
@@ -13,12 +13,12 @@ function Earth() {
         <div className={"container-fluid centra"}>
             <div className={" container-fluid w-100"}>
                 <Link to="detail" state={["Earth","dark-side-of-the-moon"]}><Album name={"Dark Side of The Moon"} artist={"Pink Floyd"} class={"dark"} cover={"./Earth/dark.jpg"}></Album></Link>
-                <Album name={"Space Oddity"} artist={"David Bowie"} class={"oddity"} cover={"./Earth/oddity.jpg"}></Album>
-                <Painting name={"Scuola di Atene"} artist={"Rafael Sanzio"} class={"scuola"} cover={"./Earth/Scuola.jpg"}></Painting>
-                <div className={"poema text-right albumtext"}>
+                <Link to="detail" state={["Earth","space-oddity"]}><Album name={"Space Oddity"} artist={"David Bowie"} class={"oddity"} cover={"./Earth/oddity.jpg"}></Album></Link>
+                <Link to="detail" state={["Earth","scuola-di-atene"]}><Painting name={"Scuola di Atene"} artist={"Rafael Sanzio"} class={"scuola"} cover={"./Earth/Scuola.jpg"}></Painting></Link>
+                <Link to="detail" state={["Earth","tabacaria"]}><div className={"poema text-right albumtext"}>
                     <Text text={"Não sou nada. || Nunca serei nada. || Não posso querer ser nada. || À parte isso, tenho em mim todos os sonhos do mundo."}
                           source={"Tabacaria, Álvaro de Campos"}></Text>
-                </div>
+                </div></Link>
                 <div>
                     <h1 className={"text-white who amiri"}> Who are we?</h1>
                 </div>
--- src/Pages/Uranus.js
+++ src/Pages/Uranus.js
@@ -4,20 +4,20 @@ import Planet from "../Components/Planet";
 import Album from "../Components/Album";
 import Painting from "../Components/Painting";
 import Text from "../Components/Text";
-
+import {Link} from 'react-router-dom';
 
 function Uranus() {
 
     return (
         <div className={"container-fluid"}>
             <div className={" container-fluid w-100"}>
-                <Album name={"A Song For Every Moon"} artist={"Bruno Major"} class={"scuola"} cover={"./Uranus/moon.jfif"}></Album>
-                <Album name={"Undercurrent"} artist={"Bill Evans"} class={"poema evans"} cover={"./Uranus/undercurrent.jpg"}></Album>
-                <Painting name={"Nighthawks"} artist={"Edward Hopper"} class={"dark mb-5"} cover={"./Uranus/nightwalks.jpg"}></Painting>
-                <div className={"oddity livro1 text-right albumtext"}>
+                <Link to="detail" state={["Uranus","a-song-for-every-moon"]}><Album name={"A Song For Every Moon"} artist={"Bruno Major"} class={"scuola"} cover={"./Uranus/moon.jfif"}></Album></Link>
+                <Link to="detail" state={["Uranus","undercurrent"]}><Album name={"Undercurrent"} artist={"Bill Evans"} class={"poema evans"} cover={"./Uranus/undercurrent.jpg"}></Album></Link>
+                <Link to="detail" state={["Uranus","nighthawks"]}><Painting name={"Nighthawks"} artist={"Edward Hopper"} class={"dark mb-5"} cover={"./Uranus/nightwalks.jpg"}></Painting></Link>
+                            <Link to="detail" state={["Uranus","eu-nunca-guardei-rebanhos"]}><div className={"oddity livro1 text-right albumtext"}>
                     <Text text={"Não tenho ambições nem desejos. || Ser poeta não é uma ambição minha. || É a minha maneira de estar sozinho."}
                           source={"Eu Nunca Guardei Rebanhos, Alberto Caeiro"}></Text>
-                </div>
+                            </div></Link>
                 <div className={"text-right"}>
                     <h1 className={"text-white who we amiri"}> We may be alone in <br/> this darkness.</h1>
                 </div>
